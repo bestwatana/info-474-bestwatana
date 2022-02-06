aedb01f195cd4f7becf1c6452e06cf9b2a4f445d
bestwatana <bestwat@uw.edu>
2022-02-05 18:56:18 -0800

    finish assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,12 @@ import { useTooltip, useTooltipInPortal, defaultStyles } from "@visx/tooltip";
 import { LegendOrdinal } from "@visx/legend";
 import { localPoint } from "@visx/event";
 import census2000 from "./census2000";
+import AreaChart from "./img/AreaChart.png";
+import ArrVsDep from "./img/ArrVsDep.png";
+import BarChart from "./img/BarChart.png";
+import DomVsIntl from "./img/DomVsIntl.png";
+import DomVsIntlPie from "./img/DomVsIntlPie.png";
+import PieChart from "./img/PieChart.png";
 
 // This code structure is from airbnb.io's Bar Stack visualization example.
 
@@ -100,7 +106,7 @@ let colors = [
   "#A1D698",
   "#B8DEAA",
   "#CCE6BD",
-  "#DEEDCF"
+  "#DEEDCF",
 ];
 
 const colorScale = scaleOrdinal({
@@ -141,9 +147,16 @@ export default function App({
   temperatureScale.range([yMax, 0]);
 
   return (
-    <div style={{ position: "relative" }}>
-      <h1>Difference in Total U.S Population Between 1900 and 2000</h1>
-      <div style={{margin: "2rem 2rem 2rem 2rem", position: "relative"}}>
+    <div style={{ 
+      position: "relative",
+      margin: "4rem",
+    }}>
+      <a href="https://github.com/bestwatana/info-474-bestwatana.git">
+        Link to GitHub Repo
+      </a>
+      <h1>Assignment 1</h1>
+      <h2>Difference in Total U.S Population Between 1900 and 2000</h2>
+      <div style={{ margin: "2rem 2rem 2rem 2rem", position: "relative" }}>
         <svg ref={containerRef} width={width - 50} height={height}>
           <rect
             x={0}
@@ -227,34 +240,195 @@ export default function App({
           </text>
         </svg>
         <div
-        style={{
-          position: "absolute",
-          top: margin.top / 2 - 10,
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-          fontSize: "14px",
-        }}
+          style={{
+            position: "absolute",
+            top: margin.top / 2 - 10,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            fontSize: "14px",
+          }}
         >
-        <text>Age Range</text>
-        <LegendOrdinal
-          scale={colorScale}
-          direction="row"
-          labelMargin="0 15px 0 0"
-        />
+          <text>Age Range</text>
+          <LegendOrdinal
+            scale={colorScale}
+            direction="row"
+            labelMargin="0 15px 0 0"
+          />
+        </div>  
       </div>
-      <p>When I look through the U.S. Population dataset that is given for this assignment, 
-        I felt like it is possible to answer the question: “What is the difference in total 
-        U.S population between 1900 and 2000?” The visualization I chose to represent the 
-        U.S Population data is a stacked bar chart comparing the total population of the two 
-        years with each bar representing different age ranges. I believe that this 
-        visualization best showcase how much more people we have in the 2000 compared to 
-        1900 and also how much life expectancy has increased. The amount of people over 80 
-        years old (represented in light green bars) are much higher in 2000 compared to that 
-        in 1900.
-
-        <a href="https://github.com/bestwatana/info-474-bestwatana.git">Link to GitHub Repo</a>
-      </p> 
+        <p>
+          When I look through the U.S. Population dataset that is given for this
+          assignment, I felt like it is possible to answer the question: “What
+          is the difference in total U.S population between 1900 and 2000?” The
+          visualization I chose to represent the U.S Population data is a
+          stacked bar chart comparing the total population of the two years with
+          each bar representing different age ranges. I believe that this
+          visualization best showcase how much more people we have in the 2000
+          compared to 1900 and also how much life expectancy has increased. The
+          amount of people over 80 years old (represented in light green bars)
+          are much higher in 2000 compared to that in 1900.
+        </p>
+      <div
+      > 
+        <h1>Assignment 2</h1>
+        <h2>Los Angeles International Airport Passenger Traffic Analysis</h2>
+        <p>
+          The “Los Angeles International Airport Passenger Traffic By Terminal”
+          dataset contains data on the number of passenger collected monthly
+          from 2006 to 2021. The annual passenger count is divided into
+          different categories and sub-categories in each row. We have 12
+          different terminals and within each terminal, the count is divided
+          into international and domestic passengers and arrival and departure
+          passengers. I believe creating visualizations based on this dataset
+          will give us insightful passenger demographics at LAX and how COVID-19
+          has impacted air travel at one of the busiest international airports
+          in the US.  
+        </p>
+        <p>
+          There are multiple types of visualization in this analysis including
+          bar charts, line charts, pie charts, and area charts. Starting off, I knew a bar chart showcasing the total
+          number of passengers in each terminal would be the best overall
+          visualization for this dataset allowing us to compare how much
+          passenger traffic each terminal has been handling over the years. I
+          have decided to divided each bar into 2 stacks to reveal how many
+          passenger in each terminal are international or domestic passenger.
+        </p>
+        <p>
+          With the given data, some visualizations are more benefitial when colors are
+          used to help differentiate the different categories. In the area chart and 
+          pie chart showing differences in numbers between multiple terminals, color 
+          helps us visually differentiate each terminal and is more effective than if
+          if the visualization is done in grayscale. 
+        </p>
+        <p
+          style={{
+            paddingBottom: "4rem",
+          }}
+        >
+          Another chart that I believe revealed important insight is the
+          various pie charts visualizing the ratio between total domestic and
+          international passengers each year at LAX. It was revealed that
+          international traffic has covered over 25% of the total traffic data
+          every year in this data until 2020. In 2021 less than 20% of
+          passengers at LAX are international passenger. This is because
+          domestic traffic at LAX has recovered at a faster rate than
+          international traffic as seen in the previous visualization. Each pie 
+          chart sizes are different allowing us to see how the number of 
+          passengers decrease or increase each year.
+        </p>
+        <img
+          src={BarChart}
+          style={{
+            width: "80%",
+          }}
+          alt="Domestic vs International by Terminal Bar Chart"
+        />
+        <p
+          style={{
+            width: "80%",
+            paddingBottom: "4rem",
+          }}
+        >
+          According to this visualization, you can see that TBIT terminal
+          handles most international passengers at LAX while other terminals
+          except TBIT West Gates and Terminal 2 handles mostly domestic
+          passengers. Overall, the TBIT terminal also handles the most
+          passengers at LAX followed by Terminal 4.
+        </p>
+        <img 
+          src={ArrVsDep}
+          style={{
+            width: "80%",
+          }} 
+          alt="Arrival vs Departure Line Chart" 
+        />
+        <p
+          style={{
+            width: "80%",
+            paddingBottom: "4rem",
+          }}
+        >
+          This line showcases the average number of arrival and departure
+          passengers each month at LAX. Overall, LAX sees more passengers during
+          the summer season. It is also noteworthy to see that the number of
+          arrival passengers is almost always slightly higher than the number of
+          departure passengers.
+        </p>
+        <img 
+          src={PieChart} 
+          style={{
+            width: "80%",
+          }}   
+          alt="Share of Passenger by Terminal Pie Chart" 
+        />
+        <p
+          style={{
+            width: "80%",
+            paddingBottom: "4rem",
+          }}
+        >
+          This pie chart showcases the share of passenger traffic at LAX by
+          terminal. Overall, Terminal 1 receives the most passengers traffic on
+          average followed by TBIT.
+        </p>
+        <img 
+          src={DomVsIntl} 
+          style={{
+            width: "80%",
+          }}            
+          alt="Domestic vs International Line Chart" 
+        />
+        <p
+          style={{
+            width: "80%",
+            paddingBottom: "4rem",
+          }}
+        >
+          Here you can understand the impact of the COVID-19 pandemic on
+          passenger count at LAX airport. The number of passengers decreased
+          significantly for both domestic and international passengers in 2020.
+          However, domestic traffic seems to be recovering at a faster rate than
+          international traffic.
+        </p>
+        <img
+          src={AreaChart}
+          style={{
+            width: "80%",
+          }}  
+          alt="Passenger Count of Each Terminal By Year Area Chart"
+        />
+        <p
+          style={{
+            width: "80%",
+            paddingBottom: "4rem",
+          }}
+        >
+          This area graph comparing passenger numbers for each terminal during
+          each year revealed the closure of Terminal 3 (dark green) in 2020 and the lack
+          of recovery from the pandemic in some terminals like Terminal 1
+          (bright orange), Terminal 4 (light green), and TBIT (red) which hasn't fully
+          grown back to its pre-pandemic traffic numbers.
+        </p>
+        <img 
+          src={DomVsIntlPie}
+          style={{
+            width: "80%",
+          }}             
+          alt="Domestic vs International Pie Chart" 
+        />
+        <p
+          style={{
+            width: "80%",
+            paddingBottom: "4rem",
+          }}
+        >
+          These various pie charts showcase the ratio between international and
+          domestic passengers each year at LAX. The number of international
+          passengers has always exceeded 25% until the pandemic hit. In 2021,
+          less than 20% of the total number of passengers at LAX are
+          international passengers.
+        </p>
       </div>
     </div>
   );
